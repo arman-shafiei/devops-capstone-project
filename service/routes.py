16b53664efca873b57eccd450b4fffd618e8dc3b
arman-shafiei <arman_shafiei_1998@yahoo.com>
2023-09-24 15:06:53 -0400

    Added Delete API

--- service/routes.py
+++ service/routes.py
@@ -113,7 +113,18 @@ def update_account(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    This endpoint will delete an Account based on the account_id
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    else:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
