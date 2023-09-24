0a5c574a18f0ca35df2cb60838a0ecba3dacbe95
arman-shafiei <arman_shafiei_1998@yahoo.com>
2023-09-24 12:53:12 -0400

    Added read API

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,20 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.get_json()
         self.assertEqual(len(response_data),0)
+
+    def test_read_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        response_data = response.get_json()
+        self.assertEqual(response_data["name"],account.name)
+
+    def test_read_account_not_found(self):
+        """It should not Read an Account that couldn't been found"""
+        response = self.client.get(
+            f"{BASE_URL}/0", content_type="application/json"
+        )
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
